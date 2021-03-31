364b718947fbdf49d3e059bfb228a4398060e9d2
RFelixRocha <felixrochaeso@gmail.com>
2021-03-30 21:21:54 -0400

    Funções para envio de notificações locais.

--- app/src/main/java/com/rfelixr/applicationnotification/MainActivity.kt
+++ app/src/main/java/com/rfelixr/applicationnotification/MainActivity.kt
@@ -2,10 +2,21 @@ package com.rfelixr.applicationnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.rfelixr.applicationnotification.utils.showNotication
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnSend: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.button_send_notification)
+
+        btnSend.setOnClickListener {
+            this.showNotication("123","Aulas Koklin","Bootcamp de andoid kotlin com a DIOS.")
+        }
     }
 }

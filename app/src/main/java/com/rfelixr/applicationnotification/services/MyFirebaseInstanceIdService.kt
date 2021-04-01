49f0cdbb05b5b34b0316e0971b01b8117b2b2486
RFelixRocha <felixrochaeso@gmail.com>
2021-04-01 19:31:56 -0400

    Add service Messaging para receber as notificações em background

--- app/src/main/java/com/rfelixr/applicationnotification/services/MyFirebaseInstanceIdService.kt
+++ app/src/main/java/com/rfelixr/applicationnotification/services/MyFirebaseInstanceIdService.kt
@@ -3,10 +3,13 @@ package com.rfelixr.applicationnotification.services
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
 
-class MyFirebaseInstanceIdService : Service() {
+class MyFirebaseInstanceIdService : FirebaseInstanceIdService() {
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    override fun onTokenRefresh() {
+        Log.i("**newTokenService",FirebaseInstanceId.getInstance().token.toString())
     }
 }

7d0e7a4a5e6c5fc9f9c30afc937efd0723f1da4e
RFelixRocha <felixrochaeso@gmail.com>
2021-04-01 19:26:36 -0400

    Add service para gerar token do firebase

---
+++ app/src/main/java/com/rfelixr/applicationnotification/services/MyFirebaseInstanceIdService.kt
@@ -0,0 +1,12 @@
+package com.rfelixr.applicationnotification.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyFirebaseInstanceIdService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
